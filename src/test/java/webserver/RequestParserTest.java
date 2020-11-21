7519ddd1514673b0dddf34f3e52fd61f73e4d5a0
kit(김광수) <kit.t@kakaocorp.com>
2020-11-21 22:32:37 +0900

    test: HttpRequest 를 생성한다

--- src/test/java/webserver/RequestParserTest.java
+++ src/test/java/webserver/RequestParserTest.java
@@ -1,9 +1,12 @@
 package webserver;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RequestParserTest {
     @Test
@@ -16,6 +19,21 @@ public class RequestParserTest {
                 + "Accept: */*"));
         //@formatter:on
         new RequestParser(bufferedReader);
+    }
+
+    @DisplayName("HttpRequest 를 생성한다")
+    @Test
+    void createHttpRequest() {
+        //@formatter:off
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(
+                "GET /index.html HTTP/1.1\n"
+                        + "Host: localhost:8080\n"
+                        + "Connection: keep-alive\n"
+                        + "Accept: */*"));
+        //@formatter:on
+        RequestParser requestParser = new RequestParser(bufferedReader);
+        HttpRequest httpRequest = requestParser.parse();
+        assertThat(httpRequest).isNotNull();
     }
 
     private static class RequestParser {
