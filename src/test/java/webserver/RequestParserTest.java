4ae868db79b274e1a3acb7b9900fed13974810a0
kit(김광수) <kit.t@kakaocorp.com>
2020-11-22 20:19:49 +0900

    refactor: Nested 구조로 변경
    동일한 객체에 대해 QueryString 을 분석하는 테스트를 추가하기 위해

--- src/test/java/webserver/RequestParserTest.java
+++ src/test/java/webserver/RequestParserTest.java
@@ -2,6 +2,7 @@ package webserver;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -13,42 +14,45 @@ public class RequestParserTest {
 
     private BufferedReader bufferedReader;
 
-    @BeforeEach
-    void setUp() {
-        //@formatter:off
-        bufferedReader = new BufferedReader(new StringReader(
-                "GET /index.html HTTP/1.1\n"
-                + "Host: localhost:8080\n"
-                + "Connection: keep-alive\n"
-                + "Accept: */*"));
-        //@formatter:on
-    }
+    @Nested
+    @DisplayName("페이지를 로드한다")
+    class PageLoad {
+        @BeforeEach
+        void setUp() {
+            //@formatter:off
+            bufferedReader = new BufferedReader(new StringReader(
+                    "GET /index.html HTTP/1.1\n"
+                            + "Host: localhost:8080\n"
+                            + "Connection: keep-alive\n"
+                            + "Accept: */*"));
+            //@formatter:on
+        }
 
-    @DisplayName("HttpRequest 를 생성한다")
-    @Test
-    void createHttpRequest() {
-        assertThat(new RequestParser(bufferedReader).parse()).isNotNull();
-    }
+        @DisplayName("HttpRequest 를 생성한다")
+        @Test
+        void createHttpRequest() {
+            assertThat(new RequestParser(bufferedReader).parse()).isNotNull();
+        }
 
-    @DisplayName("메소드를 파싱한다")
-    @Test
-    void parseMethod() {
-        HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
-        assertThat(httpRequest.getMethod()).isEqualTo("GET");
-    }
+        @DisplayName("메소드를 파싱한다")
+        @Test
+        void parseMethod() {
+            HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
+            assertThat(httpRequest.getMethod()).isEqualTo("GET");
+        }
 
-    @DisplayName("RequestURI 를 파싱한다")
-    @Test
-    void parseRequestURI() {
-        HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
-        assertThat(httpRequest.getRequestURI()).isEqualTo("/index.html");
-    }
+        @DisplayName("RequestURI 를 파싱한다")
+        @Test
+        void parseRequestURI() {
+            HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
+            assertThat(httpRequest.getRequestURI()).isEqualTo("/index.html");
+        }
 
-    @DisplayName("프로토콜을 를 파싱한다")
-    @Test
-    void parseProtocol() {
-        HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
-        assertThat(httpRequest.getProtocol()).isEqualTo("HTTP/1.1");
+        @DisplayName("프로토콜을 를 파싱한다")
+        @Test
+        void parseProtocol() {
+            HttpRequest httpRequest = new RequestParser(bufferedReader).parse();
+            assertThat(httpRequest.getProtocol()).isEqualTo("HTTP/1.1");
+        }
     }
-
 }
