f63b136c83b8cab28730d90362bc8bbe22a54d27
kit(김광수) <kit.t@kakaocorp.com>
2020-11-29 22:34:11 +0900

    test: 여러개의 컨트롤러로 매핑이 가능하다

--- src/test/java/webserver/RequestMappingTest.java
+++ src/test/java/webserver/RequestMappingTest.java
@@ -2,6 +2,11 @@ package webserver;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +17,21 @@ public class RequestMappingTest {
         IndexController indexController = new IndexController();
         RequestMapping requestMapping = new RequestMapping("/index", indexController);
         assertThat(requestMapping.getController("/index")).isEqualTo(indexController);
+    }
+
+    @DisplayName("여러개의 컨트롤러로 매핑이 가능하다")
+    @ParameterizedTest
+    @MethodSource("mappingConfig")
+    void controllerMapping(String uri, Controller controller) {
+        RequestMapping requestMapping = new RequestMapping(uri, controller);
+        assertThat(requestMapping.getController("uri")).isEqualTo(controller);
+    }
+
+    private static Stream<Arguments> mappingConfig() {
+        return Stream.of(
+                Arguments.of("/index", new IndexController()),
+                Arguments.of("/health_check", new HealthCheckController())
+        );
     }
 
     private static class IndexController {
