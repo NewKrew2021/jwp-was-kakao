045963cd7c4afdb9eaf0cf92088cf313fc0d1071
kit(김광수) <kit.t@kakaocorp.com>
2020-11-29 22:29:50 +0900

    test: make it pass

--- src/test/java/webserver/RequestMappingTest.java
+++ src/test/java/webserver/RequestMappingTest.java
@@ -3,6 +3,8 @@ package webserver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RequestMappingTest {
     @DisplayName("매핑된 컨트롤러르 리턴한다")
     @Test
@@ -10,5 +12,24 @@ public class RequestMappingTest {
         IndexController indexController = new IndexController();
         RequestMapping requestMapping = new RequestMapping("/index", indexController);
         assertThat(requestMapping.getController("/index")).isEqualTo(indexController);
+    }
+
+    private static class IndexController {
+
+    }
+
+    private class RequestMapping {
+        private final String uri;
+        private final IndexController indexController;
+
+        public RequestMapping(String uri, IndexController indexController) {
+
+            this.uri = uri;
+            this.indexController = indexController;
+        }
+
+        public IndexController getController(String uri) {
+            return indexController;
+        }
     }
 }
