28397bf29e877b836cc0eb3d16935daa1d2ca615
kit(김광수) <kit.t@kakaocorp.com>
2020-11-29 22:54:56 +0900

    refactor: 옵셔널로 변경

--- src/main/java/webserver/RequestMapping.java
+++ src/main/java/webserver/RequestMapping.java
@@ -3,26 +3,22 @@ package webserver;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
 
 class RequestMapping {
     private final Map<String, Controller> uriMapping;
 
-    public RequestMapping(String uri, Controller indexController) {
+    RequestMapping(String uri, Controller indexController) {
         this(ImmutableMap.of(uri, indexController));
     }
 
     public RequestMapping(Map<String, Controller> uriMapping) {
-
         this.uriMapping = uriMapping;
     }
 
     public Controller getController(String uri) {
-        Controller controller = uriMapping.get(uri);
-        if (Objects.isNull(controller)) {
-            throw new ControllerNotFoundException(uri);
-        }
-        return controller;
+        return Optional.ofNullable(uriMapping.get(uri))
+                .orElseThrow(() -> new ControllerNotFoundException(uri));
     }
 
 }
