893c82c00324bedbb4a6cc95f1c4b38b29453aef
Adel(양해원) <adel.yang@kakaocorp.com>
2020-11-30 11:11:13 +0900

    test:resourcePath 구하기 테스트추가

--- src/test/java/utils/RequestPathUtilsTest.java
+++ src/test/java/utils/RequestPathUtilsTest.java
@@ -2,6 +2,11 @@ package utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,5 +18,23 @@ public class RequestPathUtilsTest {
 		assertThat(RequestPathUtils.getRequestPath("GET /index.html HTTP/1.1")).isEqualTo("/index.html");
 		assertThat(RequestPathUtils.getRequestPath("GET /test HTTP/1.1")).isEqualTo("/test");
 		assertThat(RequestPathUtils.getRequestPath("")).isEqualTo("");
+	}
+
+	@ParameterizedTest
+	@DisplayName("requestPath를 기반으로 리소스 path를 구할 수 있다.")
+	@MethodSource("provideResourcePath")
+	public void getResourcePath(String requestPath, String expected) {
+		assertThat(RequestPathUtils.getResourcePath(requestPath)).isEqualTo(expected);
+	}
+
+	private static Stream<Arguments> provideResourcePath() {
+		return Stream.of(
+				Arguments.of("/index.html", "./templates/index.html"),
+				Arguments.of("/css/bootstrap.min.css", "./static/css/bootstrap.min.css"),
+				Arguments.of("/fonts/glyphicons-halflings-regular.eot", "./static/fonts/glyphicons-halflings-regular.eot"),
+				Arguments.of("/images/80-text.png", "./static/images/80-text.png"),
+				Arguments.of("/js/scripts.js", "./static/js/scripts.js"),
+				Arguments.of("/favicon.ico", "./templates/favicon.ico")
+		);
 	}
 }
