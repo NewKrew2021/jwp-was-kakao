560b387e5762ace4f9f1b049ec2e586be1ec253a
kriery <jongchan.lee@lmep.kr>
2020-12-02 09:45:08 +0900

    feat: req2: Request에 GET parameter 파싱 추가

--- src/main/java/webserver/model/Request.java
+++ src/main/java/webserver/model/Request.java
@@ -7,23 +7,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Request {
     private static final Pattern requestLinePattern = Pattern.compile(
             "(?<method>[A-Z]+)" +         // request method
-            " +(?<path>/[^?# ]+)" +        // resource path (only path part)
+            " +(?<path>/[^?# ]*)" +        // resource path (only path part)
             "(?:\\?(?<query>[^# ]+))?" +  // query string (optional)
             "(?:#(?<hash>[^ ]+))?" +      // hash (optional)
             "(?: +(?<version>.*))?"       // version (optional)
     );
+    private static final Pattern querySplitPattern = Pattern.compile("&");
+    private static final Pattern keyValuePattern = Pattern.compile("=");
 
     private final String method;
     private final String path;
     private final String query;
     private final String version;
+    private final Map<String, String> parameters;
 
     public Request(InputStream in) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -32,6 +39,7 @@ public class Request {
         path = matcher.group("path");
         query = matcher.group("query");
         version = matcher.group("version");
+        parameters = parseQuery(query);
     }
 
     private Matcher parseRequestLine(String requestLine) {
@@ -42,7 +50,24 @@ public class Request {
         return matcher;
     }
 
+    private Map<String, String> parseQuery(String query) {
+        if (Objects.isNull(query) || query.trim().isEmpty()) {
+            return Collections.emptyMap();
+        }
+        return querySplitPattern.splitAsStream(query)
+                .map(keyValuePattern::split)
+                .collect(Collectors.toMap(s -> s[0], s -> s[1], (l, r) -> l));
+    }
+
     public byte[] getRequestedResource() throws IOException, URISyntaxException {
         return FileIoUtils.loadFileFromClasspath("./templates" + path);
+    }
+
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+
+    public Map<String, String> getParameters() {
+        return Collections.unmodifiableMap(parameters);
     }
 }
--- src/test/java/webserver/model/RequestTest.java
+++ src/test/java/webserver/model/RequestTest.java
@@ -3,9 +3,11 @@ package webserver.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class RequestTest {
@@ -37,5 +39,25 @@ public class RequestTest {
         InputStream in = new ByteArrayInputStream(requestLine.getBytes());
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Request(in));
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "3 | GET /main?k1=val1&k2=val2&k3=val3",
+            "2 | GET /main?k1=val1&k2=val2&k2=val3",
+    })
+    void parameters(int expected, String requestLine) throws IOException {
+        InputStream in = new ByteArrayInputStream(requestLine.getBytes());
+        Assertions.assertThat(new Request(in).getParameters().size()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "k1 | val1 | GET /main?k1=val1&k2=val2&k3=val3",
+            "k2 | val2 | GET /main?k1=val1&k2=val2&k2=val3",
+    })
+    void parameter(String queryKey, String expected, String requestLine) throws IOException {
+        InputStream in = new ByteArrayInputStream(requestLine.getBytes());
+        Assertions.assertThat(new Request(in).getParameter(queryKey)).isEqualTo(expected);
     }
 }
