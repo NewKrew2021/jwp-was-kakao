615546036de2cd71351beadfdfbc9aa7754b4ac6
kriery <jongchan.lee@lmep.kr>
2020-12-02 23:48:29 +0900

    feat: req2: 응답 코드(HttpStatus) 추가

---
+++ src/main/java/webserver/model/HttpStatus.java
@@ -0,0 +1,22 @@
+package webserver.model;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int status, String message) {
+        this.code = status;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
