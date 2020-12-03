afc7192ef00f83ae1a03e2852c9d03ebe8f19390
belle.bonjour <belle.bonjour@kakaocorp.com>
2020-12-03 23:20:05 +0900

    Feature : response 변경

--- README.md
+++ README.md
@@ -28,3 +28,10 @@
 * 로그인이 성공하면 index.html로 이동하고, 로그인이 실패하면 /user/login_failed.html로 이동해야 한다.
 * 로그인이 성공하면 cookie를 활용해 로그인 상태를 유지할 수 있어야 한다. 
 * 로그인이 성공할 경우 요청 header의 Cookie header 값이 logined=true, 로그인이 실패하면 Cookie header 값이 logined=false로 전달되어야 한다.
+
+#### 요구사항 6)
+* 접근하고 있는 사용자가 “로그인” 상태일 경우(Cookie 값이 logined=true) 경우 http://localhost:8080/user/list 로 접근했을 때 사용자 목록을 출력한다.
+* 만약 로그인하지 않은 상태라면 로그인 페이지(login.html)로 이동한다.
+
+#### 요구사항 7)
+* Stylesheet 파일을 지원하도록 구현 한다.
--- src/main/java/webserver/ResponseHandler.java
+++ src/main/java/webserver/ResponseHandler.java
@@ -1,6 +1,12 @@
 package webserver;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
 import utils.FileIoUtils;
+
+import java.io.IOException;
 
 public class ResponseHandler {
 
@@ -14,6 +20,10 @@ public class ResponseHandler {
         }
     }
 
+    public static boolean isStaticFile(String path) {
+        return path.contains(".html");
+    }
+
     public static String getViewPath(String path) {
         if (path.startsWith("/css")
                 || path.startsWith("/fonts")
@@ -22,5 +32,23 @@ public class ResponseHandler {
             return "./static" + path;
         }
         return "./templates" + path + "";
+    }
+
+    public static String createTemplatesView(String location, Object data) {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = null;
+        try {
+            template = handlebars.compile(location);
+            return template.apply(data);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
