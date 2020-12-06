e9c2fae74bb62d8e06792fb980744b8f8977783c
kit(김광수) <kit.t@kakaocorp.com>
2020-12-06 16:02:57 +0900

    feat: 매핑 URI에 메소드 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -14,9 +14,9 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
     private static final RequestMapping REQUEST_MAPPING = new RequestMapping(ImmutableMap.of(
-            "/usr/create", new CreateUserController(),
-            "/user/login", new LoginController(),
-            "/user/list", new UserListController()));
+            "GET /usr/create", new CreateUserController(),
+            "POST /user/login", new LoginController(),
+            "GET /user/list", new UserListController()));
 
     public static void main(String[] args) throws Exception {
         int port = 0;
