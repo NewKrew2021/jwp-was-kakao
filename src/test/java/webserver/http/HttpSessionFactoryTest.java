9c539cbda602338fc9f3ce747f04c6a985a207fc
kit(김광수) <kit.t@kakaocorp.com>
2020-12-10 21:40:31 +0900

    refactor: 중복제거

--- src/test/java/webserver/http/HttpSessionFactoryTest.java
+++ src/test/java/webserver/http/HttpSessionFactoryTest.java
@@ -1,6 +1,7 @@
 package webserver.http;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,38 +18,37 @@ import static org.assertj.core.api.Assertions.assertThat;
  * * 세션아이디가 존재하지 않거나 널이면 새로운 객체를 리턴한다
  */
 public class HttpSessionFactoryTest {
-    @Test
-    void createSession() {
-        HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
-        assertThat(httpSessionFactory.create(null)).isInstanceOf(HttpSession.class);
+
+    private HttpSessionFactory httpSessionFactory;
+
+    @BeforeEach
+    void setUp() {
+        httpSessionFactory = new HttpSessionFactory();
     }
 
     @DisplayName("세션 아이디가 널이면 새로운 세션을 생성한다")
     @Test
     void createSessionWithNullId() {
-        HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
-        assertThat(httpSessionFactory.create(null)).isNotEqualTo(httpSessionFactory.create(null));
+        assertThat(httpSessionFactory.getOrCreate(null)).isNotEqualTo(httpSessionFactory.getOrCreate(null));
     }
 
     @DisplayName("세션 아이디가 같으면 같은 객체를 반환한다")
     @Test
     void createSessionWithExistingId() {
-        HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
         String id = "session id";
-        assertThat(httpSessionFactory.create(id)).isEqualTo(httpSessionFactory.create(id));
+        assertThat(httpSessionFactory.getOrCreate(id)).isEqualTo(httpSessionFactory.getOrCreate(id));
     }
 
     @DisplayName("주어진 세션아이디를 가진 세션객체를 생성한다")
     @Test
     void createSessionWithGivenId() {
-        HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
         String id = "session id";
-        assertThat(httpSessionFactory.create(id).getId()).isEqualTo(id);
+        assertThat(httpSessionFactory.getOrCreate(id).getId()).isEqualTo(id);
     }
 
     private static class HttpSessionFactory {
         private final Map<String, HttpSession> sessions = new HashMap<>();
-        public HttpSession create(String sessionId) {
+        public HttpSession getOrCreate(String sessionId) {
             String actualSessionId = sessionId;
             if (actualSessionId == null) {
                 actualSessionId = UUID.randomUUID().toString();
