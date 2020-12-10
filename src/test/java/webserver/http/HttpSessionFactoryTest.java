5abf7f08222ef0f4c295e82a49236a9a14ff0c82
kit(김광수) <kit.t@kakaocorp.com>
2020-12-10 21:31:42 +0900

    test: 세션 아이디가 널이면 새로운 세션을 생성한다

--- src/test/java/webserver/http/HttpSessionFactoryTest.java
+++ src/test/java/webserver/http/HttpSessionFactoryTest.java
@@ -1,6 +1,7 @@
 package webserver.http;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * v 세션객체를 생성한다
  * * 세션아이디가 존재하면 동일한 객체를 리턴한다
+ * * 주어진 세션아이디를 가진 세션객체를 생성한다.
  * * 세션아이디가 존재하지 않거나 널이면 새로운 객체를 리턴한다
  */
 public class HttpSessionFactoryTest {
@@ -15,6 +17,13 @@ public class HttpSessionFactoryTest {
     void createSession() {
         HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
         assertThat(httpSessionFactory.create(null)).isInstanceOf(HttpSession.class);
+    }
+
+    @DisplayName("세션 아이디가 널이면 새로운 세션을 생성한다")
+    @Test
+    void createSessionWithNullId() {
+        HttpSessionFactory httpSessionFactory = new HttpSessionFactory();
+        assertThat(httpSessionFactory.create(null)).isNotEqualTo(httpSessionFactory.create(null));
     }
 
     private static class HttpSessionFactory {
