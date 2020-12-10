256120b932a9858c5eb2d5cd519733bc1da2dc3b
kit(김광수) <kit.t@kakaocorp.com>
2020-12-10 20:48:16 +0900

    feat: make it pass

--- src/test/java/webserver/http/HttpSessionTest.java
+++ src/test/java/webserver/http/HttpSessionTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * * String getId(): 현재 세션에 할당되어 있는 고유한 세션 아이디를 반환
+ * v String getId(): 현재 세션에 할당되어 있는 고유한 세션 아이디를 반환
  * * void setAttribute(String name, Object value): 현재 세션에 value 인자로 전달되는 객체를 name 인자 이름으로 저장
  * * Object getAttribute(String name): 현재 세션에 name 인자로 저장되어 있는 객체 값을 찾아 반환
  * * void removeAttribute(String name): 현재 세션에 name 인자로 저장되어 있는 객체 값을 삭제
@@ -29,10 +29,17 @@ public class HttpSessionTest {
 
     private static class HttpSession {
 
+        private final String id;
+
         public HttpSession(String id) {
+            this.id = id;
             if (id == null) {
                 throw new IllegalArgumentException();
             }
+        }
+
+        public String getId() {
+            return id;
         }
     }
 }
