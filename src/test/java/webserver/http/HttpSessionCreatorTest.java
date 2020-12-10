3cb0992a3b9fb380be3de22da5eba4e1e31ae973
kit(김광수) <kit.t@kakaocorp.com>
2020-12-10 21:25:25 +0900

    test: make it pass

--- src/test/java/webserver/http/HttpSessionCreatorTest.java
+++ src/test/java/webserver/http/HttpSessionCreatorTest.java
@@ -3,6 +3,8 @@ package webserver.http;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * * 세션 아이디를 생성한다
  * * 새션객체를 세션아이디와 함께 생성한다
@@ -12,5 +14,11 @@ public class HttpSessionCreatorTest {
     void createSession() {
         HttpSessionCreator httpSessionCreator = new HttpSessionCreator();
         assertThat(httpSessionCreator.create()).isInstanceOf(HttpSession.class);
+    }
+
+    private static class HttpSessionCreator {
+        public HttpSession create() {
+            return new HttpSession("");
+        }
     }
 }
