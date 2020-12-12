85a3c43ab43495a7d201e89d1953fc19b7f08701
genos.lee <genos.lee@kakaocorp.com>
2020-12-12 21:33:58 +0900

    refactor: service에 전달하는 파라미터는 request를 그대로 전달하지 않도록 수정

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,12 +1,11 @@
 package service;
 
 import db.DataBase;
-import model.User;
+import model.user.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
-import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,12 +23,7 @@ public class UserServiceTest {
     public void saveUser() {
         User expectedUser = new User("javajigi2", "password", "%EB%B0%95%EC%9E%AC%EC%84%B1", "javajigi%40slipp.net");
 
-        User user = service.addUser(new HashMap<String, String>() {{
-            put("userId", "javajigi2");
-            put("password", "password");
-            put("name", "%EB%B0%95%EC%9E%AC%EC%84%B1");
-            put("email", "javajigi%40slipp.net");
-        }});
+        User user = service.addUser(expectedUser);
 
         assertThat(user).isEqualTo(expectedUser);
         assertThat(DataBase.findUserById("javajigi2")).isEqualTo(expectedUser);
@@ -38,30 +32,21 @@ public class UserServiceTest {
 
     @Test
     public void loginUser() {
-        boolean login = service.login(new HashMap<String, String>() {{
-            put("userId", "javajigi");
-            put("password", "password");
-        }});
+        boolean login = service.login("javajigi", "password");
 
         assertThat(login).isEqualTo(true);
     }
 
     @Test
     public void loginNoUser() {
-        boolean login = service.login(new HashMap<String, String>() {{
-            put("userId", "javajig");
-            put("password", "password");
-        }});
+        boolean login = service.login("javajig", "password");
 
         assertThat(login).isEqualTo(false);
     }
 
     @Test
     public void loginInvalidPassword() {
-        boolean login = service.login(new HashMap<String, String>() {{
-            put("userId", "javajigi");
-            put("password", "passwod");
-        }});
+        boolean login = service.login("javajigi", "passwod");
 
         assertThat(login).isEqualTo(false);
     }
