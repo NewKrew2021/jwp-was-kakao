d7a05a494cdcff790186eba5e5a6d954dfcdd794
tars.case <tars.case@kakaocorp.com>
2021-01-29 15:17:22 +0900

    feat : 원하는 path로 이동

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,12 +22,27 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+
+            String line = bufferedReader.readLine();
+            String[] parsed = line.split(" ");
+
+            String method = parsed[0];
+            String path = "./templates" + parsed[1];
+
+            byte[] body = FileIoUtils.loadFileFromClasspath(path);
+            while(line == null || line.equals("")) {
+                String[] list = line.split(" ");
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
+            logger.error(e.getMessage());
+        } catch (URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
