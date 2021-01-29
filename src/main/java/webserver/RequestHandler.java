ccfef37ad504efd395f1905e288554080dc08fb2
tars.case <tars.case@kakaocorp.com>
2021-01-29 18:09:07 +0900

    feat : 유저 생성 시 리다이렉트

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,21 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.net.URISyntaxException;
-import java.util.List;
-
+import annotation.web.Controller;
 import dto.RequestHeader;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static final String BASE_URL = "http://localhost:8080/index.html";
 
     private Socket connection;
 
@@ -31,7 +34,7 @@ public class RequestHandler implements Runnable {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
             String request = "";
             String line = bufferedReader.readLine();
-            while(line != null && !line.equals("")) {
+            while (line != null && !line.equals("")) {
                 request += line + "\n";
                 line = bufferedReader.readLine();
             }
@@ -39,20 +42,22 @@ public class RequestHandler implements Runnable {
 
             List<String> parameters = requestHeader.getParameters();
 
-            if(requestHeader.getMethod().equals("POST")) {
+            if (requestHeader.getMethod().equals("POST")) {
                 String body = bufferedReader.readLine();
                 parameters = RequestParser.parseUserInfo(body);
             }
 
             User user = null;
+            DataOutputStream dos = new DataOutputStream(out);
             if (requestHeader.getPath().equals("/user/create")) {
                 user = new User(parameters.get(0), parameters.get(1), parameters.get(2), parameters.get(3));
+                response302Header(dos, BASE_URL);
                 logger.debug(user.toString());
+                return;
             }
 
             byte[] body = FileIoUtils.loadFileFromClasspath("./templates" + requestHeader.getPath());
 
-            DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -67,6 +72,16 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+            dos.writeBytes("Location: " + location + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
