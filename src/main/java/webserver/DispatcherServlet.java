997d71e10f6363dff01382362d4839749498c2e4
danial.yoo <danial.yoo@kakaocorp.com>
2021-01-29 19:40:22 +0900

    change - user controller 유저 생성 api 스펙 변경 -> get 에서 post로, query params 에서 body params로

--- src/main/java/webserver/DispatcherServlet.java
+++ src/main/java/webserver/DispatcherServlet.java
@@ -5,11 +5,14 @@ import dto.HttpRequest;
 public class DispatcherServlet {
 
     public static byte[] run(HttpRequest request){
-        if(request.getMethod().equals("GET")){
+        if(request.getMethod().equals("POST")){
             // /user 이면
             if(request.getUri().startsWith("/user/create")){
                 return UserController.create(request);
             }
+        }
+
+        if(request.getMethod().equals("GET")){
             // 파일 요청이면
             if(request.getUri().endsWith(".html")){
                 return FileController.get(request);
--- src/main/java/webserver/UserController.java
+++ src/main/java/webserver/UserController.java
@@ -8,10 +8,10 @@ import java.nio.charset.StandardCharsets;
 
 public class UserController {
     public static byte[] create(HttpRequest request) {
-        String userId = request.getQuery().get("userId");
-        String password = request.getQuery().get("password");
-        String name = request.getQuery().get("name");
-        String email = request.getQuery().get("email");
+        String userId = request.getBodyParams().get("userId");
+        String password = request.getBodyParams().get("password");
+        String name = request.getBodyParams().get("name");
+        String email = request.getBodyParams().get("email");
 
         DataBase.addUser(new User(userId, password, name, email));
         return "성공".getBytes(StandardCharsets.UTF_8);
