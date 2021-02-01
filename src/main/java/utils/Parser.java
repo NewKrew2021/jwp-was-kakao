ba9f2bfa00b48ed51fea3b4a6edd54fcf5d88aa5
Sangyeol Kim <syeol.kim@daum.net>
2021-02-01 16:36:31 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -3,22 +3,9 @@ package utils;
 import webserver.HttpMethod;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Parser {
-    
-    public static Map<String, String> parseHeaderFromRequestMessage(List<String> requestMessage) {
-        Map<String, String> header = new HashMap<>();
-        for(String line : requestMessage) {
-            if (line.equals("")) { // TO DO: indent 줄이기
-                break;
-            }
-            String[] data = line.split(": ");
-            header.put(data[0], data[1]);
-        }
-        return header;
-    }
 
     public static HttpMethod parseMethodFromRequestLine(String requestLine) {
         String method = requestLine.split(" ")[0];
@@ -29,19 +16,13 @@ public class Parser {
         return requestLine.split(" ")[1];
     }
 
-    public static String parseBodyFromRequestMessage(List<String> requestMessage) {
-
-
-        return null;
-    }
-
     public static Map<String, String> parseUserParams(String query) {
         int beginIdx = query.indexOf("?");
         String[] params = query.substring(beginIdx + 1)
                 .split("&");
 
         Map<String, String> userParams = new HashMap<>();
-        for(String param : params) {
+        for (String param : params) {
             String[] data = param.split("=");
             userParams.put(data[0], data[1]);
         }
