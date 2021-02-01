2b45fa961c2234a381102eaa2b7d681db9807c62
Sangyeol Kim <syeol.kim@daum.net>
2021-02-01 14:35:02 +0900

    feat: 사용자 회원가입 기능 추가

--- src/main/java/webserver/UserController.java
+++ src/main/java/webserver/UserController.java
@@ -6,24 +6,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.Parser;
 
-import java.util.List;
 import java.util.Map;
 
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    public static void handleUser(HttpMethod method, String url, String body) {
-        if(method == HttpMethod.GET && url.contains("/create")) {
+    public static String handle(HttpMethod method, String url, String body) {
+        if (method == HttpMethod.GET && url.contains("/create")) {
             Map<String, String> params = Parser.parseUserParams(url);
             User user = User.from(params);
-            logger.debug(user.toString());
+            logger.debug("[" + method.name() + "] create " + user.toString());
             DataBase.addUser(user);
+            return "./templates/index.html";
         }
-        if(method == HttpMethod.POST && url.contains("/create")) {
+        if (method == HttpMethod.POST && url.contains("/create")) {
             Map<String, String> params = Parser.parseUserParams(body);
             User user = User.from(params);
-            logger.debug(user.toString());
+            logger.debug("[" + method.name() + "] create " + user.toString());
             DataBase.addUser(user);
+            return "./templates/index.html";
         }
+        return "./templates/error.html";
     }
 }
--- src/test/java/webserver/UserControllerTest.java
+++ src/test/java/webserver/UserControllerTest.java
@@ -1,25 +1,46 @@
 package webserver;
 
+import db.DataBase;
+import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("사용자 관련 기능")
 public class UserControllerTest {
 
     @DisplayName("GET 메서드로 사용자 정보를 받으면 사용자 정보를 디비에 저장한다.")
     @Test
-    void handleUser() {
+    void handler() {
         // given
-        String url =
-        UserController.handleUser(HttpMethod.GET, url, header);
+        HttpMethod method = HttpMethod.GET;
+        String url = "/user/create?userId=jordy&password=1q2w3e4r!&name=죠르디&email=jordy@kakaocorp.com";
+        String body = "";
+        User expected = User.of("jordy", "1q2w3e4r!", "죠르디", "jordy@kakaocorp.com");
+
+        // when
+        UserController.handle(method, url, body);
+        User actual = DataBase.findUserById("jordy");
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("POST 메서드로 사용자 정보를 받으면 사용자 정보를 디비에 저장한다.")
     @Test
-    void handleUser2() {
+    void handle2() {
         // given
+        HttpMethod method = HttpMethod.POST;
+        String url = "/user/create";
+        String body = "userId=jordy&password=1q2w3e4r!&name=죠르디&email=jordy@kakaocorp.com";
+        User expected = User.of("jordy", "1q2w3e4r!", "죠르디", "jordy@kakaocorp.com");
 
-
+        // when
+        UserController.handle(method, url, body);
+        User actual = DataBase.findUserById("jordy");
 
         // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
