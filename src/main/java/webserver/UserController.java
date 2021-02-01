a3d2c27cfd7c057bf784e0d2c84eb0526e1bc063
danial.yoo <danial.yoo@kakaocorp.com>
2021-02-01 15:19:19 +0900

    feature - 회원가입 완료시 index.html 리다이렉트

--- src/main/java/webserver/UserController.java
+++ src/main/java/webserver/UserController.java
@@ -2,18 +2,22 @@ package webserver;
 
 import db.DataBase;
 import dto.HttpRequest;
+import dto.HttpResponse;
 import model.User;
 
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserController {
-    public static byte[] create(HttpRequest request) {
+    public static HttpResponse create(HttpRequest request) {
         String userId = request.getBodyParams().get("userId");
         String password = request.getBodyParams().get("password");
         String name = request.getBodyParams().get("name");
         String email = request.getBodyParams().get("email");
 
         DataBase.addUser(new User(userId, password, name, email));
-        return "성공".getBytes(StandardCharsets.UTF_8);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", "http://localhost:8080/index.html");
+        return new HttpResponse("HTTP/1.1 302 Found", headers);
     }
 }
