e92c03b7c817792d59e5ca89e20d35c8117baa42
waylon <waylon.n@kakaocorp.com>
2021-02-02 18:54:39 +0900

    refactor: Http Request/Response 객체 수정
    변경 전:
        Request - line, header, body로 구분
        Response - 각 status별 객체 분리
    변경 후:
        Request - method, path, header, parameters로 구분
        Response - status enum 사용 및 reponse 통합

---
+++ src/main/java/webserver/HttpStatusCode.java
@@ -0,0 +1,31 @@
+package webserver;
+
+public enum HttpStatusCode {
+
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found");
+
+    private int code;
+    private String description;
+
+    HttpStatusCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + description;
+    }
+}
