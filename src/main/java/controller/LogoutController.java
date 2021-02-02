0830c8a3656395db33232388f36e872a570fea64
echo.ohce <echo.ohce@kakaocorp.com>
2021-02-02 18:31:53 +0900

    feat: 로그아웃 기능 추가 구현

--- src/main/java/controller/LogoutController.java
+++ src/main/java/controller/LogoutController.java
@@ -1,4 +1,14 @@
 package controller;
 
-public class LogoutController {
+import request.HttpRequest;
+import response.HttpResponse;
+
+public class LogoutController extends AbstractController {
+    private static final String INDEX_HTML = "/index.html";
+    private static final String FALSE = "false";
+
+    @Override
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
+        httpResponse.response302Header(INDEX_HTML, FALSE);
+    }
 }
