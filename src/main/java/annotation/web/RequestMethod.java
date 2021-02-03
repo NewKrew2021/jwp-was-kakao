676bd3515d85a7a566dd8affab455d7d5ca9de75
jamal.park <jamal.park@kakaocorp.com>
2021-02-03 15:30:54 +0900

    refactor: 불필요 코드 정리, README.md 수정

--- src/main/java/annotation/web/RequestMethod.java
+++ src/main/java/annotation/web/RequestMethod.java
@@ -1,21 +1,5 @@
 package annotation.web;
 
-import java.util.Arrays;
-
 public enum RequestMethod {
-    GET("GET"), HEAD("HEAD"), POST("POST"), PUT("PUT"), PATCH("PATCH"), DELETE("DELETE"), OPTIONS("OPTIONS"), TRACE("TRACE"), NOTHING("NOTHING");
-
-    private String method;
-
-    RequestMethod(String method) {
-        this.method = method;
-    }
-
-    public static RequestMethod getMethod(String line) {
-        String[] parsedLine = line.split(" ");
-        return Arrays.stream(values())
-                .filter(it -> parsedLine[0].equals(it.method))
-                .findAny()
-                .orElse(RequestMethod.NOTHING);
-    }
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
 }
