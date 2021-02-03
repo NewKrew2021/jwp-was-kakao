59cc5c5593c4288121e3061955c3d85d3e60b6f3
echo.ohce <echo.ohce@kakaocorp.com>
2021-02-03 17:47:44 +0900

    refactor: index 상수 처리

--- src/main/java/request/RequestUri.java
+++ src/main/java/request/RequestUri.java
@@ -11,6 +11,8 @@ import static utils.HttpRequestUtils.*;
 
 
 public class RequestUri {
+    private static final int METHOD_INDEX_IN_LINE = 0;
+    private static final int URI_INDEX_IN_LINE = 1;
     private RequestMethod requestMethod;
     private String path;
     private Map<String, String> params;
@@ -23,15 +25,19 @@ public class RequestUri {
 
     public static RequestUri from(String line) {
         String[] splitLine = line.split(" ");
-        Optional<String> queryString = extractParams(splitLine[1]);
+        Optional<String> queryString = extractParams(splitLine[URI_INDEX_IN_LINE]);
         if (queryString.isPresent()) {
-            return new RequestUri(getMethodType(splitLine[0]), extractPath(splitLine[1]), requestStringToMap(queryString.get()));
+            return new RequestUri(getMethodType(splitLine[METHOD_INDEX_IN_LINE])
+                    , extractPath(splitLine[URI_INDEX_IN_LINE])
+                    , requestStringToMap(queryString.get()));
         }
-        return new RequestUri(getMethodType(splitLine[0]), extractPath(splitLine[1]), new HashMap<>());
+        return new RequestUri(getMethodType(splitLine[METHOD_INDEX_IN_LINE])
+                , extractPath(splitLine[URI_INDEX_IN_LINE])
+                , new HashMap<>());
     }
 
     private static RequestMethod getMethodType(String line) {
-        String method = line.split(" ")[0];
+        String method = line.split(" ")[METHOD_INDEX_IN_LINE];
         if (method.equals("GET")) {
             return RequestMethod.GET;
         }
@@ -56,7 +62,6 @@ public class RequestUri {
         }
         return Optional.empty();
     }
-
 
     public RequestMethod getRequestMethod() {
         return requestMethod;
