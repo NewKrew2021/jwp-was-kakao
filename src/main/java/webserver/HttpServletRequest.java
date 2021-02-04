0aaffcba7aa67e9d3042d2130f7665198961f7e5
Ye-Ji-Kim <kyeoji0128@naver.com>
2021-02-04 10:39:00 +0900

    feat: HttpServletRequest 객체 구현

---
+++ src/main/java/webserver/HttpServletRequest.java
@@ -0,0 +1,17 @@
+package webserver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpServletRequest {
+    public static Map<String, HttpSession> sessions = new HashMap<>();
+
+    public static HttpSession getSession(){
+        HttpSession session = new HttpSession();
+        sessions.put(session.getId(), session);
+        return session;
+    }
+    public static HttpSession getSession(String sessionId){
+        return sessions.get(sessionId);
+    }
+}
