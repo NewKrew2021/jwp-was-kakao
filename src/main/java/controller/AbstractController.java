7ba9ac4a0c2d43378593ab892126561b0e9d9a84
kuri <denver5636@naver.com>
2021-02-04 13:58:51 +0900

    [refactor] get, post외 다른 http method들어올 경우 고려

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -10,11 +10,12 @@ import java.net.URISyntaxException;
 public class AbstractController implements Controller {
 
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        if ("GET".equals(httpRequest.getMethod())) {
+        if (httpRequest.getMethod().equals("GET")) {
             doGet(httpRequest, httpResponse);
-            return;
         }
-        doPost(httpRequest, httpResponse);
+        if (httpRequest.getMethod().equals("POST")) {
+            doPost(httpRequest, httpResponse);
+        }
     }
 
     public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
