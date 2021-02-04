84bf848e5776d1ff03a7a59461ae812a22fe6d2b
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-02-04 16:37:53 +0900

    refactor(controller) : url path와 controller 관계를 enum으로 정의

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -6,18 +6,8 @@ import response.HttpResponse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
 
 public abstract class AbstractController implements Controller {
-
-    private static final Map<String, Controller> controllers = new HashMap<>();
-
-    static {
-        controllers.put("/user/list", new ListUserController());
-        controllers.put("/user/create",new CreateUserController());
-        controllers.put("/user/login", new LoginController());
-    }
 
     @Override
     public void service(HttpRequest request, HttpResponse response)  throws IOException, URISyntaxException {
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,25 +1,17 @@
 package controller;
 
+import controller.common.UrlPath;
 import request.HttpRequest;
 import response.HttpResponse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.Map;
 
 public interface Controller {
 
     Controller defaultController = new DefaultController();
-    Map<String, Controller> controllers = putMapping();
-
-    static Map<String, Controller> putMapping() {
-        Map<String, Controller> controllers = new HashMap<>();
-        controllers.put("/user/list", new ListUserController());
-        controllers.put("/user/create", new CreateUserController());
-        controllers.put("/user/login", new LoginController());
-        return controllers;
-    }
+    Map<String, Controller> controllers = UrlPath.getControllers();
 
     static Controller of(String path) {
         Controller controller = controllers.get(path);
