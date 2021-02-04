317e5f54c8aceea17cbcabf068bdf82da4aae7bf
limdef <lim.s.jin22@gmail.com>
2021-02-04 15:52:22 +0900

    refactor: controllerMap 선언과 동시에 초기화 되도록 변경

--- src/main/java/controller/ControllerMapper.java
+++ src/main/java/controller/ControllerMapper.java
@@ -4,19 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ControllerMapper {
-    public static Map<String, Controller> controllerMap;
+    public static final Map<String, Controller> CONTROLLER_MAP = new HashMap<>();
 
-    private static void init() {
-        controllerMap = new HashMap<>();
-        controllerMap.put("/user/create", new CreateUserController());
-        controllerMap.put("/user/list", new ListUserController());
-        controllerMap.put("/user/login", new LoginController());
+    static {
+        CONTROLLER_MAP.put("/user/create", new CreateUserController());
+        CONTROLLER_MAP.put("/user/list", new ListUserController());
+        CONTROLLER_MAP.put("/user/login", new LoginController());
     }
 
     public static Controller get(String path) {
-        if (controllerMap == null) {
-            init();
-        }
-        return controllerMap.get(path);
+        return CONTROLLER_MAP.get(path);
     }
 }
