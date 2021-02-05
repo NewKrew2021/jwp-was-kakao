e910d7c2301efe9539fde2aa6776c3000dbcc5a1
Sangyeol Kim <syeol.kim@daum.net>
2021-02-05 20:35:22 +0900

    refactor: 기능과 책임 분리

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,85 +1,85 @@
 package webserver;
 
+import error.ErrorResponse;
+import model.MIME;
+import model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.ResourceLoader;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
-    protected static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private static final String HTTP_VERSION = "HTTP/1.1";
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private static final String ERROR_PAGE = "<!DOCTYPE html><html lang=\"kr\"><body><h1>%s</h1><h2>%s</h2></body></html>";
 
-    private final OutputStream outputStream;
-    private final Map<String, String> header = new HashMap<>();
-    private byte[] body = new byte[0];
-    private HttpStatusCode statusCode;
+    private final HttpStatus httpStatus;
+    private final Map<String, String> header;
+    private final byte[] body;
 
-    public HttpResponse(OutputStream outputStream) {
-        this.outputStream = outputStream;
+    private HttpResponse(HttpStatus httpStatus, Map<String, String> header, byte[] body) {
+        this.httpStatus = httpStatus;
+        this.header = header;
+        this.body = body;
+    }
+
+    private HttpResponse(HttpStatus httpStatus, Map<String, String> header) {
+        this(httpStatus, header, new byte[0]);
+    }
+
+    private HttpResponse(HttpStatus httpStatus, byte[] body) {
+        this(httpStatus, new HashMap<>(), body);
+    }
+
+    public static HttpResponse redirect(String url) {
+        Map<String, String> header = new HashMap<>();
+        header.put("Location", url);
+        return new HttpResponse(HttpStatus.FOUND, header);
+    }
+
+    public static HttpResponse ok(Resource resource) {
+        Map<String, String> header = new HashMap<>();
+        header.put("Content-Type", MIME.getContentType(resource.getExtension()));
+        return new HttpResponse(HttpStatus.OK, header, resource.getBytes());
+    }
+
+    public static HttpResponse error(ErrorResponse errorResponse) {
+        logger.error(errorResponse.getMessage());
+        String errorPage = String.format(ERROR_PAGE, errorResponse.getHttpStatus().toString(), errorResponse.getMessage());
+        HttpResponse response = new HttpResponse(errorResponse.getHttpStatus(), errorPage.getBytes(StandardCharsets.UTF_8));
+        response.addHeader("Content-Type", MIME.getContentType("html"));
+        return response;
     }
 
     public void addHeader(String key, String value) {
         header.put(key, value);
     }
 
-    public void forward(String path) {
-        forwardBody(ResourceLoader.getBytes(path));
+    public byte[] getBody() {
+        return body;
     }
 
-    public void forwardBody(byte[] body) {
-        this.body = body;
-        statusCode = HttpStatusCode.OK;
-    }
-
-    public void sendRedirect(String url) {
-        addHeader("Location", url);
-        statusCode = HttpStatusCode.FOUND;
-    }
-
-    public void badRequest() {
-        statusCode = HttpStatusCode.BAD_REQUEST;
-    }
-
-    public void notFound() {
-        statusCode = HttpStatusCode.NOT_FOUND;
-    }
-
-    public void unauthorized() {
-        statusCode = HttpStatusCode.UNAUTHORIZED;
-    }
-
-    public void writeResponse() {
-        try {
-            writeResponseLine();
-            writeResponseHeader();
-            writeResponseBody();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void writeResponseLine() throws IOException {
-        write(RequestHandler.HTTP_VERSION_NAME + " " + statusCode.toString() + "\r\n");
-    }
-
-    private void writeResponseHeader() throws IOException {
-        for (Map.Entry<String, String> entry : header.entrySet()) {
-            write(entry.getKey() + ": " + entry.getValue() + "\r\n");
-        }
-        write("\r\n");
-    }
-
-    private void writeResponseBody() throws IOException {
-        outputStream.write(body, 0, body.length);
-        outputStream.flush();
-    }
-
-    private void write(String message) throws IOException {
+    public void logResponseHeader() {
+        String message = "\n========= Response Header =========\n"
+                + getHeader();
         logger.debug(message);
-        outputStream.write(message.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String getHeader() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(HTTP_VERSION)
+                .append(" ")
+                .append(httpStatus.toString())
+                .append("\r\n");
+        for (Map.Entry<String, String> entry : header.entrySet()) {
+            sb.append(entry.getKey())
+                    .append(": ")
+                    .append(entry.getValue())
+                    .append("\r\n");
+        }
+        sb.append("\r\n");
+        return sb.toString();
     }
 }
