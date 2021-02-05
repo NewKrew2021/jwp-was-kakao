1ec366a719e6d891dea4bc34818226571714539e
Sangyeol Kim <syeol.kim@daum.net>
2021-02-05 20:34:18 +0900

    style: 네이밍 변경

--- src/main/java/webserver/HttpStatus.java
+++ src/main/java/webserver/HttpStatus.java
@@ -1,17 +1,18 @@
 package webserver;
 
-public enum HttpStatusCode {
-
+public enum HttpStatus {
     OK(200, "OK"),
     FOUND(302, "Found"),
     BAD_REQUEST(400, "Bad Request"),
     UNAUTHORIZED(401, "Unauthorized"),
-    NOT_FOUND(404, "Not Found");
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error")
+    ;
 
-    private int code;
-    private String description;
+    private final int code;
+    private final String description;
 
-    HttpStatusCode(int code, String description) {
+    HttpStatus(int code, String description) {
         this.code = code;
         this.description = description;
     }
