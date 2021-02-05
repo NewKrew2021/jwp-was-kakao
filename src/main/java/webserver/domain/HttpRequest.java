880976244e99fe9e72950d9f98ff1258ddcbbb7f
ocean.world <ocean.world@kakaocorp.com>
2021-02-05 12:18:13 +0900

    refactor : request parsing 시 패턴 매칭 사용하도록 리팩토링

--- src/main/java/webserver/domain/HttpRequest.java
+++ src/main/java/webserver/domain/HttpRequest.java
@@ -1,80 +1,105 @@
 package webserver.domain;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.util.ObjectUtils;
 import utils.IOUtils;
 
 import java.io.*;
 import java.net.URLDecoder;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpRequest {
-    private static final String BODY_SEPARATOR = "&|=";
-    private static final String PARAMETER_SEPARATOR = "\\?";
-    private static final String FIRST_LINE_SEPARATOR = " ";
-    private static final String HEADER_SEPARATOR = ": ";
-    private static final String DEFAULT_ENCODING = "UTF-8";
-    private static final String CONTENT_LENGTH = "Content-Length";
-    private static final String HTTP_VERSION = "Version";
+    private static final Pattern bodyPattern = Pattern.compile("&");
+    private static final Pattern headerPattern = Pattern.compile(" *: *");
+    private static final Pattern keyValuePattern = Pattern.compile("=");
+    private static final Pattern requestLinePattern = Pattern.compile(
+            "(?<method>[A-Z]+)" +         // request method
+                    " +(?<path>/[^?# ]*)" +        // resource path (only path part)
+                    "(?:\\?(?<query>[^# ]+))?" +  // query string (optional)
+                    "(?: +(?<version>.*))?"       // version (optional)
+    );
 
+    private static final String DEFAULT_ENCODING = "UTF-8";
+    private static final String LOGIN_COOKIE = "logined=true";
+    private static final String DEFAULT_LOGIN_COOKIE = "logined=false";
+    private static final String DEFAULT_CONTENT_LENGTH = "0";
 
     private Map<String, String> httpHeader = new HashMap<>();
     private Map<String, String> parameters = new HashMap<>();
     private HttpMethod httpMethod;
     private String path;
+    private String version;
 
     public HttpRequest(InputStream in) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
 
         String line = bufferedReader.readLine();
         parseFirstLine(line);
+
         parseHeader(bufferedReader);
-        parseBody(bufferedReader);
+
+        int bodyLength = Integer.parseInt(httpHeader.getOrDefault(HttpHeaders.CONTENT_LENGTH, DEFAULT_CONTENT_LENGTH));
+        String body = IOUtils.readData(bufferedReader, bodyLength);
+        parseBody(body);
     }
 
-    private void parseBody(BufferedReader bufferedReader) throws IOException {
-        int bodyLength = Integer.parseInt(httpHeader.getOrDefault(CONTENT_LENGTH, "0"));
-        if (bodyLength == 0) {
-            return;
+    private void parseFirstLine(String line) {
+        Matcher matcher = requestLinePattern.matcher(line);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid request line");
         }
-        String body = IOUtils.readData(bufferedReader, bodyLength);
-        String[] parameters = body.split(BODY_SEPARATOR);
-        for (int i = 0; i < parameters.length; i = i + 2) {
-            this.parameters.put(parameters[i], URLDecoder.decode(parameters[i + 1], DEFAULT_ENCODING));
+
+        httpMethod = HttpMethod.resolve(matcher.group("method"));
+        path = matcher.group("path");
+        version = matcher.group("version");
+        String query = matcher.group("query");
+
+        if (!ObjectUtils.isEmpty(query)) {
+            parseBody(query);
         }
     }
 
     private void parseHeader(BufferedReader bufferedReader) throws IOException {
         String line = bufferedReader.readLine();
         while (line != null && !line.equals("")) {
-            String[] currentLine = line.split(HEADER_SEPARATOR);
-            httpHeader.put(currentLine[0], URLDecoder.decode(currentLine[1], DEFAULT_ENCODING));
+            String[] splitted = headerPattern.split(line);
+            putHeader(splitted[0], splitted[1]);
             line = bufferedReader.readLine();
         }
     }
 
-    private void parseFirstLine(String line) throws UnsupportedEncodingException {
-        String[] currentLine = line.split(FIRST_LINE_SEPARATOR);
-        httpMethod = HttpMethod.resolve(currentLine[0]);
-
-        String[] pathAndParameter = currentLine[1].split(PARAMETER_SEPARATOR);
-        path = pathAndParameter[0];
-
-        if (pathAndParameter.length > 1) {
-            makeParameter(pathAndParameter[1].split(BODY_SEPARATOR));
-        }
-
-        httpHeader.put(HTTP_VERSION, currentLine[2]);
+    private void parseBody(String body) {
+        String[] keyValues = bodyPattern.split(body);
+        Arrays.stream(keyValues)
+                .map(keyValue -> keyValuePattern.split(keyValue))
+                .filter(keyValue -> keyValue.length == 2)
+                .forEach(keyValue -> putParameter(keyValue[0], keyValue[1]));
     }
 
-    private void makeParameter(String[] parameters) throws UnsupportedEncodingException {
-        for (int i = 0; i < parameters.length; i = i + 2) {
-            this.parameters.put(parameters[i], URLDecoder.decode(parameters[i + 1], DEFAULT_ENCODING));
+    private void putParameter(String key, String value) {
+        try {
+            parameters.put(key, URLDecoder.decode(value, DEFAULT_ENCODING));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void putHeader(String key, String value) {
+        try {
+            httpHeader.put(key, URLDecoder.decode(value, DEFAULT_ENCODING));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
     }
 
     public boolean isLogined() {
-        return httpHeader.get("Cookie").contains("logined=true");
+        return httpHeader.getOrDefault(HttpHeaders.COOKIE, DEFAULT_LOGIN_COOKIE).contains(LOGIN_COOKIE);
     }
 
     public HttpMethod getMethod() {
@@ -83,6 +108,10 @@ public class HttpRequest {
 
     public String getPath() {
         return path;
+    }
+
+    public String getVersion() {
+        return version;
     }
 
     public String getHeader(String key) {
