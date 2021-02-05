147522bc928390208790eceb137e162953f602a8
sierra.s <sierra.s@kakaocorp.com>
2021-02-05 13:42:46 +0900

    fix: Controller 인터페이스 메서드를 추상메서드로 변경

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,6 +7,5 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface Controller {
-    default void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-    }
+    void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException;
 }
