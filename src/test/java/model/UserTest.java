f702af8c48722a72109a79baf2547ad483f33472
teo.kkim <teo.kkim@kakaocorp.com>
2021-03-05 15:47:19 +0900

    refactor : 불필요한 예외 핸들링 코드 제거

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -3,8 +3,6 @@ package model;
 import domain.HttpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -15,26 +13,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UserTest {
 
-    private Logger logger = LoggerFactory.getLogger(UserTest.class.getName());
-
     @Test
     @DisplayName("유저를 올바르게 생성")
-    void create_user() {
+    void create_user() throws IOException {
         String input = "GET /user/create?userId=javajigi&password=password&name=자바지기&email=javajigi%40slipp.net HTTP/1.1\n" +
                 "Host: localhost:8080\n" +
                 "Connection: keep-alive\n" +
                 "Accept: *\n";
 
         InputStream stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
-        try {
-            HttpRequest httpRequest = HttpRequest.from(stream);
-            User user = new User(httpRequest.getParameters());
-            assertThat(user.getUserId()).isEqualTo("javajigi");
-            assertThat(user.getPassword()).isEqualTo("password");
-            assertThat(user.getName()).isEqualTo("자바지기");
-            assertThat(user.getEmail()).isEqualTo("javajigi%40slipp.net");
-        } catch (IOException e) {
-            logger.info(e.getMessage());
-        }
+
+        HttpRequest httpRequest = HttpRequest.from(stream);
+        User user = new User(httpRequest.getParameters());
+        assertThat(user.getUserId()).isEqualTo("javajigi");
+        assertThat(user.getPassword()).isEqualTo("password");
+        assertThat(user.getName()).isEqualTo("자바지기");
+        assertThat(user.getEmail()).isEqualTo("javajigi%40slipp.net");
     }
 }
